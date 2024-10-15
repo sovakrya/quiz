98a3993b2f5a749b794ae7c073a79380ed95f188
sovaKrya <tyan_34ee@mail.ru>
2024-10-15 13:10:42 +0300

    added createNewTask and checkResult in TheGame

--- src/components/TheGame.tsx
+++ src/components/TheGame.tsx
@@ -1,42 +1,119 @@
+import { useRef, useState } from "react";
 import "../styles/TheGame.css";
+import { useNavigate } from "react-router-dom";
 
 export default function TheGame() {
-  const example = window.game.createExample();
-  const result = window.game.countExample(example);
+  let example = window.game.createExample();
+  let result = window.game.countExample(example);
+  const [exampleDone, setExampleDone] = useState(false);
+  const [exampleCorrect, setExampleCorrect] = useState(false);
+  const [countTask, setCountTask] = useState(1);
+  const exampleElements = useRef<HTMLDivElement | null>(null);
 
-  function a() {
-    console.log(example);
-    console.log(result);
+  const navigate = useNavigate();
 
-    example.map((val) => console.log(val));
+  function goBack() {
+    navigate("/");
+  }
+
+  function checkResult() {
+    const userExample: Array<number | string> = [];
+    const elements = exampleElements.current?.children;
+    if (!elements) {
+      return;
+    }
+    const example: Element[] = Array.from(elements);
+
+    example.map((elem, idx) => {
+      if (elem instanceof HTMLInputElement) {
+        if (isNaN(Number(elem.value))) {
+          alert(
+            `There is no number in the input field the number: ${
+              idx + 1
+            }! You can only enter numbers`
+          );
+        }
+
+        userExample.push(Number(elem.value));
+      }
+
+      if (elem instanceof HTMLSpanElement) {
+        userExample.push(elem.textContent!);
+      }
+    });
+
+    const res = eval(userExample.join(""));
+    if (res === result) {
+      setExampleDone(true);
+      setExampleCorrect(true);
+    } else {
+      setExampleDone(true);
+      setExampleCorrect(false);
+    }
+
+    example.map((val) => {
+      if (val instanceof HTMLInputElement) {
+        val.value = "";
+      }
+    });
+  }
+
+  function createNewTask() {
+    example = window.game.createExample();
+    result = window.game.countExample(example);
+
+    setExampleDone(false);
+    setCountTask(countTask + 1);
   }
 
   return (
     <div className="game-main-box">
       <div className="header-box">
-        <span>#</span>
+        <span>#{countTask}</span>
 
-        <button className="header-btn">Закончить</button>
+        <button className="header-btn" onClick={goBack}>
+          Закончить
+        </button>
       </div>
 
       <div className="game-content-box">
-        <div>
-          {example.map((val) => {
-            if (typeof val === "number") {
-              return <input type="number" />;
-            }
+        <div className="example-box">
+          <div className="example-wrapper" ref={exampleElements}>
+            {example.map((val) => {
+              if (typeof val === "number") {
+                return <input type="number" className="example-input" />;
+              }
 
-            if (typeof val === "string") {
-              return <span>{val}</span>;
-            }
-          })}
+              if (typeof val === "string") {
+                return <span className="example-sign">{val}</span>;
+              }
+            })}
+          </div>
 
-          <span>{`= ${result}`}</span>
+          <div className="example-result-box">
+            <span>=</span>
+            <span>{result}</span>
+          </div>
         </div>
-        <button>Ответить</button>
-        <span>Правильно/Неправильно</span>
-        <span>Вариант ответа</span>
-        <button onClick={a}>d</button>
+
+        <button onClick={checkResult}>Ответить</button>
+        {exampleDone ? (
+          <button onClick={createNewTask}>Следующий</button>
+        ) : (
+          <></>
+        )}
+        {exampleDone ? (
+          <div>
+            {exampleCorrect ? <span>Правильно</span> : <span>Неправильно</span>}
+            <div>
+              <span>{"Можно так:"}</span>
+              <span>{example}</span>
+              <span>{`= ${result}`}</span>
+            </div>
+          </div>
+        ) : (
+          <></>
+        )}
       </div>
     </div>
   );
